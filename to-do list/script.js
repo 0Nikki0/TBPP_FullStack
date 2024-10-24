9e8387bd089b83c78ba88c7154ba676cd42445a6
0Nikki0 <nikitachaudharyagra@gmail.com>
2024-10-24 20:44:47 +0530

    Add to-do list folder

---
+++ to-do list/script.js
@@ -0,0 +1,5 @@
+const addbutton = document.getElementById('id');
+const del = document.getElementById('id');
+const root = document.getElementById('id');
+const taskname = document.getElementById('id');
+const taskdesc = document.getElementById('id');
